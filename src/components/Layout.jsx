2a74eff5aee53ea15055afae1b839c7be64d1ab1
HassanRagab494 <169075197+HassanRagab494@users.noreply.github.com>
2025-08-30 00:12:00 +0300

    Update Layout.jsx

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,16 +1,21 @@
-// Layout.jsx
 import React, { useState } from "react";
 import Sidebar from "./Sidebar";
 import Navbar from "./Navbar";
 
 function Layout({ children }) {
   const [open, setOpen] = useState(true);
-  console.log("Layout - setOpen (from useState):", setOpen); 
-  console.log("Layout - Type of setOpen (from useState):", typeof setOpen);
 
   return (
     <div className="d-flex">
-      <Sidebar open={open} setOpen={setOpen} />
+      <div className={`sidebar-container ${open ? "open" : "collapsed"}`}>
+        <Sidebar open={open} setOpen={setOpen} />
+      </div>
+
+
+      <div className="flex-grow-1">
+        <Navbar setOpen={setOpen} />
+        <main className="p-3">{children}</main>
+      </div>
     </div>
   );
 }
