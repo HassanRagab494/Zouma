5a33c9e6080f9fd64e5d1787ea681662d4c53ac7
HassanRagab494 <169075197+HassanRagab494@users.noreply.github.com>
2026-07-04 13:55:49 +0300

    Remove unused useCallback import in ClientsPhoneBook

--- src/pages/ClientsPhoneBook.jsx
+++ src/pages/ClientsPhoneBook.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useCallback } from "react";
+import React, { useEffect, useState } from "react";
 import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
 import { db } from "../firebaseConfig";
 
